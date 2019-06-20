a16a3c5683a332656199286e075665aab267599f
phuongnguyen <phuongnguyen952501@gmail.com>
2019-06-20 18:27:03 +0700

    Updates

---
+++ precache-manifest.a718dfac0d70a7c1f9e9eabb20fd6125.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f165c29f9f5041bda622b62de5f1902",
+    "url": "/reactCustomerList/index.html"
+  },
+  {
+    "revision": "ef552773ffe5b639d420",
+    "url": "/reactCustomerList/static/css/main.bfee1c69.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/reactCustomerList/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "ef552773ffe5b639d420",
+    "url": "/reactCustomerList/static/js/main.013a4d4b.chunk.js"
+  },
+  {
+    "revision": "94324db9744b7d999a54",
+    "url": "/reactCustomerList/static/js/runtime~main.bbeac52e.js"
+  }
+]);
